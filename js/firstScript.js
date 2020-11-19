536919e3cd750e6c1c03ae5d1bfe734da57f773a
Lucian Ailenei <lucian.ailenei@hindawi.com>
2020-11-19 19:01:17 +0200

    js example

---
+++ js/firstScript.js
@@ -0,0 +1,15 @@
+var firstValue = 10;
+
+var secondValue = 20;
+
+var computedValue = firstValue + secondValue;
+
+// console.log("CHECK IF IT IS DISPLAYED IN THE CONSOLE");
+
+console.log("REZULTATUL ESTE:", computedValue);
+
+// lower camel case: lowerCamelCase
+// upper camel case: UpperCamelCase
+// snake case: snake_case
+
+// var numberOfPotatoesKilos = 10
