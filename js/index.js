dfe445cbf5dbec702f3a4a4c369caa42648d210d
Lucian Ailenei <lucian.ailenei@hindawi.com>
2021-01-28 20:53:17 +0200

    localstorage example

--- js/index.js
+++ js/index.js
@@ -1,15 +1,33 @@
 import makeRequest from "./fetchModule.js";
 
-makeRequest("https://jsonplaceholder.typicode.com/users").then((response) => {
-  const mappedResult = response.map((item) => {
-    const {
-      username,
-      name,
-      address: { city },
-    } = item;
+// makeRequest("https://jsonplaceholder.typicode.com/users").then((response) => {
+//   const mappedResult = response.map((item) => {
+//     const {
+//       username,
+//       name,
+//       address: { city },
+//     } = item;
 
-    return { username, name, city };
+//     return { username, name, city };
+//   });
+
+//   console.log(mappedResult);
+// });
+
+const constructWorld = (worlds) => {
+  // construim domul cu elementele corespunzatoare
+};
+
+const retrieveFromLocalStorage = localStorage.getItem("worlds");
+
+if (retrieveFromLocalStorage) {
+  // il folosim direct ca sa creem elementele
+  constructWorld(JSON.parse(retrieveFromLocalStorage));
+} else {
+  makeRequest("../js/worlds.json").then((response) => {
+    const localStorageValue = JSON.stringify(response);
+    localStorage.setItem("worlds", localStorageValue);
+
+    constructWorld(response);
   });
-
-  console.log(mappedResult);
-});
+}
